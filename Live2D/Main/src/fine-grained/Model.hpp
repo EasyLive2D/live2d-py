3b2fbc28225667ffa48768c11f41e3395ed8cf96
Arkueid <thetardis@qq.com>
2026-01-16 19:51:30 +0800

    [v3][Std] Use right pointer alignment
    Signed-off-by: Arkueid <thetardis@qq.com>

--- Live2D/Main/src/fine-grained/Model.hpp
+++ Live2D/Main/src/fine-grained/Model.hpp
@@ -30,9 +30,9 @@ public:
      * @brief
      * @param filePath model3.json path
      */
-    void LoadModelJson(const char* filePath);
+    void LoadModelJson(const char *filePath);
 
-    const char* GetModelHomeDir();
+    const char *GetModelHomeDir();
 
     // update
 
@@ -60,7 +60,7 @@ public:
     // param
     int GetParameterCount();
 
-    void GetParameterIds(void* collector, void (*collect)(void* collector, const char* id));
+    void GetParameterIds(void *collector, void (*collect)(void *collector, const char *id));
 
     float GetParameterValue(int index);
 
@@ -70,19 +70,19 @@ public:
 
     float GetParameterDefaultValue(int index);
 
-    void SetParameterValue(const char* id, float value, float weight = 1.0f);
+    void SetParameterValue(const char *id, float value, float weight = 1.0f);
 
     void SetParameterValue(int index, float value, float weight = 1.0f);
 
-    void AddParameterValue(const char* id, float value);
+    void AddParameterValue(const char *id, float value);
 
     void AddParameterValue(int index, float value);
 
-    void SetAndSaveParameterValue(const char* id, float value, float weight = 1.0f);
+    void SetAndSaveParameterValue(const char *id, float value, float weight = 1.0f);
 
     void SetAndSaveParameterValue(int index, float value, float weight = 1.0f);
 
-    void AddAndSaveParameterValue(const char* id, float value);
+    void AddAndSaveParameterValue(const char *id, float value);
 
     void AddAndSaveParameterValue(int index, float value);
 
@@ -103,30 +103,30 @@ public:
 
     void SetScaleY(float scaleY);
 
-    const float* GetMvp();
+    const float *GetMvp();
 
     // motion
-    void StartMotion(const char* group, int no, int priority = 3, void* startCallee = nullptr,
+    void StartMotion(const char *group, int no, int priority = 3, void *startCallee = nullptr,
                      ACubismMotion::BeganMotionCallback startCalleeHandler = nullptr,
-                     void* finishCallee = nullptr,
+                     void *finishCallee = nullptr,
                      ACubismMotion::FinishedMotionCallback finishCalleeHandler = nullptr);
 
-    void StartRandomMotion(const char* group = nullptr, int priority = 3,
-                           void* startCallee = nullptr,
+    void StartRandomMotion(const char *group = nullptr, int priority = 3,
+                           void *startCallee = nullptr,
                            ACubismMotion::BeganMotionCallback startCalleeHandler = nullptr,
-                           void* finishCallee = nullptr,
+                           void *finishCallee = nullptr,
                            ACubismMotion::FinishedMotionCallback finishCalleeHandler = nullptr);
 
     bool IsMotionFinished();
 
-    int LoadExtraMotion(const char* group, const char* motionJsonPath);
+    int LoadExtraMotion(const char *group, const char *motionJsonPath);
 
     int GetMotionGroupCount();
 
-    int GetMotionCount(const char* group);
+    int GetMotionCount(const char *group);
 
-    void GetMotions(void* collector, void (*collect)(void* collector, const char* group, int no,
-                                                     const char* file, const char* sound));
+    void GetMotions(void *collector, void (*collect)(void *collector, const char *group, int no,
+                                                     const char *file, const char *sound));
 
     // reset motions
     void StopAllMotions();
@@ -136,15 +136,15 @@ public:
     void ResetPose();
 
     // mouse interaction
-    void HitPart(float x, float y, void* collector,
-                 void (*collect)(void* collector, const char* id), bool topOnly = false);
+    void HitPart(float x, float y, void *collector,
+                 void (*collect)(void *collector, const char *id), bool topOnly = false);
 
-    void HitDrawable(float x, float y, void* collector,
-                     void (*collect)(void* collector, const char* id), bool topOnly = false);
+    void HitDrawable(float x, float y, void *collector,
+                     void (*collect)(void *collector, const char *id), bool topOnly = false);
 
     void Drag(float x, float y);
 
-    bool IsAreaHit(const char* areaName, float x, float y);
+    bool IsAreaHit(const char *areaName, float x, float y);
 
     bool IsPartHit(int index, float x, float y);
 
@@ -159,31 +159,31 @@ public:
 
     // part
     int GetPartCount();
-    void GetPartIds(void* collector, void (*collect)(void* collector, const char* id));
+    void GetPartIds(void *collector, void (*collect)(void *collector, const char *id));
     void SetPartOpacity(int index, float opacity);
     void SetPartScreenColor(int index, float r, float g, float b, float a);
     void SetPartMultiplyColor(int index, float r, float g, float b, float a);
 
     // drawable
     int GetDrawableCount();
-    void GetDrawableIds(void* collector, void (*collect)(void* collector, const char* id));
+    void GetDrawableIds(void *collector, void (*collect)(void *collector, const char *id));
 
-    const float* GetDrawableVertices(int index);
+    const float *GetDrawableVertices(int index);
     const int GetDrawableVertexCount(int index);
     const int GetDrawableVertexIndexCount(int index);
-    const unsigned short* GetDrawableIndices(int index);
+    const unsigned short *GetDrawableIndices(int index);
 
     void SetDrawableMultiColor(int index, float r, float g, float b, float a);
     void SetDrawableScreenColor(int index, float r, float g, float b, float a);
 
     // expression
-    void AddExpression(const char* expressionId);
+    void AddExpression(const char *expressionId);
 
-    void RemoveExpression(const char* expressionId);
+    void RemoveExpression(const char *expressionId);
 
-    void SetExpression(const char* expressionId);
+    void SetExpression(const char *expressionId);
 
-    const char* SetRandomExpression();
+    const char *SetRandomExpression();
 
     void ResetExpressions();
 
@@ -191,15 +191,15 @@ public:
 
     int GetExpressionCount();
 
-    void GetExpressions(void* collector,
-                        void (*collect)(void* collector, const char* id, const char* file));
+    void GetExpressions(void *collector,
+                        void (*collect)(void *collector, const char *id, const char *file));
 
-    void LoadExtraExpression(const char* expressionId, const char* expressionJsonPath);
+    void LoadExtraExpression(const char *expressionId, const char *expressionJsonPath);
 
     // sizes
-    void GetCanvasSize(float& w, float& h);
+    void GetCanvasSize(float &w, float &h);
 
-    void GetCanvasSizePixel(float& w, float& h);
+    void GetCanvasSizePixel(float &w, float &h);
 
     float GetPixelsPerUnit();
 
@@ -216,29 +216,29 @@ private:
 
     void SetupTextures();
 
-    void PreloadMotionGroup(const csmChar* group);
+    void PreloadMotionGroup(const csmChar *group);
 
     void SetupModel();
 
     bool IsHit(CubismIdHandle drawableId, csmFloat32 pointX, csmFloat32 pointY) override;
 
 private:
-    ICubismModelSetting* _modelSetting;
+    ICubismModelSetting *_modelSetting;
     csmVector<CubismIdHandle> _eyeBlinkIds;
     csmVector<CubismIdHandle> _lipSyncIds;
 
     csmString _modelHomeDir;
-    csmMap<Csm::csmString, ACubismMotion*> _motions;
-    csmMap<Csm::csmString, ACubismMotion*> _expressions;
-    std::unordered_map<std::string, CubismExpressionMotionManager*> _expManagers;
+    csmMap<Csm::csmString, ACubismMotion *> _motions;
+    csmMap<Csm::csmString, ACubismMotion *> _expressions;
+    std::unordered_map<std::string, CubismExpressionMotionManager *> _expManagers;
 
 
-    const Csm::CubismId* _idParamAngleX;
-    const Csm::CubismId* _idParamAngleY;
-    const Csm::CubismId* _idParamAngleZ;
-    const Csm::CubismId* _idParamBodyAngleX;
-    const Csm::CubismId* _idParamEyeBallX;
-    const Csm::CubismId* _idParamEyeBallY;
+    const Csm::CubismId *_idParamAngleX;
+    const Csm::CubismId *_idParamAngleY;
+    const Csm::CubismId *_idParamAngleZ;
+    const Csm::CubismId *_idParamBodyAngleX;
+    const Csm::CubismId *_idParamEyeBallX;
+    const Csm::CubismId *_idParamEyeBallY;
 
     int _ParamAngleXi;
     int _ParamAngleYi;
@@ -254,9 +254,9 @@ private:
     csmFloat32 _dragX;
     csmFloat32 _dragY;
 
-    int* _tmpOrderedDrawIndice;
-    const float* _parameterDefaultValues;
-    float* _parameterValues;
+    int *_tmpOrderedDrawIndice;
+    const float *_parameterDefaultValues;
+    float *_parameterValues;
     int _parameterCount;
 
     std::vector<csmString> _motionGroupNames;
