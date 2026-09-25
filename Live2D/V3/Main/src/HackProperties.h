369bbc5e794118cdc452f3b8601544663aef479c
Arkueid <thetardis@qq.com>
2026-09-25 22:41:06 +0800

    remove unused definitions
    Signed-off-by: Arkueid <thetardis@qq.com>

--- Live2D/V3/Main/src/HackProperties.h
+++ Live2D/V3/Main/src/HackProperties.h
@@ -6,28 +6,4 @@
     std::string group; \
     int no;
 
-#define __ADDITIONAL_METHODS__ \
-    void AddAndSaveParameterValue(const Csm::CubismId* parameterId, float value, float weight = 1.0f)\
-    {\
-        const csmInt32 index = GetParameterIndex(parameterId);\
-        AddParameterValue(index, value, weight);\
-        _savedParameters[index] = _parameterValues[index];\
-    }\
-    void SetAndSaveParameterValue(const Csm::CubismId* parameterId, float value, float weight = 1.0f)\
-    {\
-        const csmInt32 index = GetParameterIndex(parameterId);\
-        SetParameterValue(index, value, weight);\
-        _savedParameters[index] = _parameterValues[index];\
-    }\
-    void AddAndSaveParameterValue(int index, float value, float weight = 1.0f)\
-    {\
-        AddParameterValue(index, value, weight);\
-        _savedParameters[index] = _parameterValues[index];\
-    }\
-    void SetAndSaveParameterValue(int index, float value, float weight = 1.0f)\
-    {\
-        SetParameterValue(index, value, weight);\
-        _savedParameters[index] = _parameterValues[index];\
-    }\
-
 #endif // HACKPROPERTIES_H
